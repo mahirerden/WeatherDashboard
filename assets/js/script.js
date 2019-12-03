a0362a16eed4532fd6bd5b45aaa51185442efa64
Mahir Erden <mahirerden@gmail.com>
2019-12-02 21:22:46 -0500

    http changed to https

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,7 +75,7 @@ $(document).ready(function () {
      }
 
      function displayLocation(latitude, longitude) {
-          var queryURL = `http://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&lat=${latitude}&lon=${longitude}`;
+          var queryURL = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&lat=${latitude}&lon=${longitude}`;
           $.ajax({
                url: queryURL,
                method: "GET"
@@ -87,7 +87,7 @@ $(document).ready(function () {
 
      function getWeatherDetails() {
           cityName = $("#search").val().trim();
-          var queryURLWeather = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+          var queryURLWeather = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
           $.ajax({
                url: queryURLWeather,
                method: "GET",
@@ -111,7 +111,7 @@ $(document).ready(function () {
                     lat = response.coord.lat;
                     dt = response.dt;
                     currentDate = new Date(dt * 1000);
-                    var imageUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`
+                    var imageUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`
                     $("#cityImg").attr("src", imageUrl);
                     $("#cityName").text(cityName + " " + currentDate.toLocaleDateString());
                     $("#temp").html("Temperature: " + tempF + " &#8457;");
@@ -130,7 +130,7 @@ $(document).ready(function () {
 
      function getUVIndex() {
                     cnt = 1;
-                    var queryURLUvi = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}&cnt=${cnt}`;
+                    var queryURLUvi = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}&cnt=${cnt}`;
                     $.ajax({
                          url: queryURLUvi,
                          method: "GET"
@@ -143,7 +143,7 @@ $(document).ready(function () {
 
      function get5DayForecast() {
                     cnt = 5;
-                    var queryURLForecast = `http://api.openweathermap.org/data/2.5/forecast?id=${cityId}&appid=${apiKey}`;
+                    var queryURLForecast = `https://api.openweathermap.org/data/2.5/forecast?id=${cityId}&appid=${apiKey}`;
                     $.ajax({
                          url: queryURLForecast,
                          method: "GET"
@@ -159,27 +159,27 @@ $(document).ready(function () {
                               objArray.push(obj);
                          }
                          $("#11").text(new Date(objArray[0].dt * 1000).toLocaleDateString());
-                         $("#12").attr("src", `http://openweathermap.org/img/wn/${objArray[0].icon}@2x.png`);
+                         $("#12").attr("src", `https://openweathermap.org/img/wn/${objArray[0].icon}@2x.png`);
                          $("#13").html("Temp: " + (((objArray[0].temp - 273.15) * 1.80 + 32).toFixed(2)) + " &#8457;");
                          $("#14").text("Humidity: " + objArray[0].humidity + " %");
 
                          $("#21").text(new Date(objArray[1].dt * 1000).toLocaleDateString());
-                         $("#22").attr("src", `http://openweathermap.org/img/wn/${objArray[1].icon}@2x.png`);
+                         $("#22").attr("src", `https://openweathermap.org/img/wn/${objArray[1].icon}@2x.png`);
                          $("#23").html("Temp: " + (((objArray[1].temp - 273.15) * 1.80 + 32).toFixed(2)) + " &#8457;");
                          $("#24").text("Humidity: " + objArray[1].humidity + " %");
 
                          $("#31").text(new Date(objArray[2].dt * 1000).toLocaleDateString());
-                         $("#32").attr("src", `http://openweathermap.org/img/wn/${objArray[2].icon}@2x.png`);
+                         $("#32").attr("src", `https://openweathermap.org/img/wn/${objArray[2].icon}@2x.png`);
                          $("#33").html("Temp: " + (((objArray[2].temp - 273.15) * 1.80 + 32).toFixed(2)) + " &#8457;");
                          $("#34").text("Humidity: " + objArray[2].humidity + " %");
 
                          $("#41").text(new Date(objArray[3].dt * 1000).toLocaleDateString());
-                         $("#42").attr("src", `http://openweathermap.org/img/wn/${objArray[3].icon}@2x.png`);
+                         $("#42").attr("src", `https://openweathermap.org/img/wn/${objArray[3].icon}@2x.png`);
                          $("#43").html("Temp: " + (((objArray[3].temp - 273.15) * 1.80 + 32).toFixed(2)) + " &#8457;");
                          $("#44").text("Humidity: " + objArray[3].humidity + " %");
 
                          $("#51").text(new Date(objArray[4].dt * 1000).toLocaleDateString());
-                         $("#52").attr("src", `http://openweathermap.org/img/wn/${objArray[4].icon}@2x.png`);
+                         $("#52").attr("src", `https://openweathermap.org/img/wn/${objArray[4].icon}@2x.png`);
                          $("#53").html("Temp: " + (((objArray[4].temp - 273.15) * 1.80 + 32).toFixed(2)) + " &#8457;");
                          $("#54").text("Humidity: " + objArray[4].humidity + " %");
                     });
